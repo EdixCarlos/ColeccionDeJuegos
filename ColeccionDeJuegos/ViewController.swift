e61753aaeb098f0156da7dd6097ba442f06514c9
EdixDante <Edix.carlos@tecsup.edu.pe>
2021-05-12 13:48:58 -0700

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Dante Carlos on 5/12/21.
+//  Copyright © 2021 Dante Carlos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
